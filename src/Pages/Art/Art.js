cd4efe6a6cf4b8944717f6c5a079d1f66838108a
tayeful912 <kazitayefulislam12@gmail.com>
2022-06-24 20:25:37 +0600

    json data created

---
+++ src/Pages/Art/Art.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Art = () => {
+    return (
+        <div>
+
+            <div>
+                <h1>Summer Art Camp! 5 Days of Artists and Painting Monet, Van Gogh, Matisse, & More </h1>
+            </div>
+
+
+
+
+        </div>
+    );
+};
+
+export default Art;
