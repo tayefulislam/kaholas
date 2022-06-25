d72e64da61bc8987c9218071dd8ac77e92419255
tayeful912 <kazitayefulislam12@gmail.com>
2022-06-25 22:24:18 +0600

    detail section added

--- src/Pages/Art/Art.js
+++ src/Pages/Art/Art.js
@@ -1,12 +1,57 @@
 import React from 'react';
+import Details from '../Details/Details';
 
 const Art = ({ art }) => {
 
-    console.log(art)
+    // console.log(art)
 
 
     return (
         <div>
+
+            {/* heading part */}
+
+            <div>
+                <h1 className='text-3xl font-bold '>{art.headTitle}</h1>
+                <p>{art?.smallTitle}</p>
+
+            </div>
+
+
+            {/* deatail part */}
+
+            <div>
+
+                <div>
+                    {/* detail section */}
+
+                    <Details
+                        art={art}
+                    >
+
+
+                    </Details>
+
+
+
+
+
+                </div>
+
+
+                <div>
+
+                    {/* picture section */}
+
+
+                </div>
+
+
+            </div>
+
+
+
+
 
 
 
---
+++ src/Pages/Details/Details.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Details = ({ art }) => {
+
+    console.log(art)
+
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Details;
